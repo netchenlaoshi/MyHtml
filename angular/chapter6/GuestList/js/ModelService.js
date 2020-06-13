0c8aa74707460362db684c3d9cc6586928b781f0
szp <2593119609@qq.com>
2020-06-13 16:31:44 +0800

    28suzhongpei

---
+++ angular/chapter6/GuestList/js/ModelService.js
@@ -0,0 +1,23 @@
+var model = angular.module('nameList.model', []);
+
+function Guest(name, phone) {
+    this.name = name;
+    this.phone = phone;
+}
+
+model.factory('modelService', function () {
+    var guestList = {
+        list: [],
+        add: function (name, phone) {
+            var guest = new Guest(name,phone);
+            this.list.push(guest);
+        },
+        getList : function () {
+            return this.list.filter(function (){
+                return true;
+            })
+            }
+
+    }
+    return guestList;
+})
