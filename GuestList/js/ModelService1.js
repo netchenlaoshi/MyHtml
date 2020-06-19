338a83d0a16e99269503f20b1061175f5dcaffa9
ASHRekindle <2099750696@qq.com>
2020-06-19 10:17:59 +0800

    作业

--- GuestList/js/ModelService1.js
+++ GuestList/js/ModelService1.js
@@ -1,25 +1,11 @@
+
 var model = angular.module('nameList.model', []);
-
-function Guest(name, phone) {
-    this.name = name;
-    this.phone = phone;
-    this.state = Guest.INVITE;
-}
-
-Guest.ACCEPT = '已接受';
-Guest.prototype.accept = function () {
-    this.state = Guest.ACCEPT;
-}
 model.factory('modelService', function () {
     var guestList = {
         list: [],
         add: function (name, phone) {
             var guest = new Guest(name, phone);
             this.list.push(guest);
-            return {
-                code: 0,
-                guest: guest
-            }
         },
 
         remove: function (guest) {
@@ -28,16 +14,29 @@ model.factory('modelService', function () {
             })
         },
 
-        getList:function(state){
-            if (state==Guest.ALL){
-                return this.list.filter(function(){
+        getList: function (state) {
+            if (state == Guest.ALL) {
+                return this.list.filter(function () {
                     return true;
                 })
             }
-            return this.list.filter(function (guest){
-                return guest.state==state;
+            return this.list.filter(function (guest) {
+                return guest.state == state;
             })
         }
     }
     return guestList;
 })
+function Guest(name, phone) {
+    this.name = name;
+    this.phone = phone;
+    this.state = Guest.INVITE;
+} Guest.INVITE = '邀请中';
+Guest.ACCEPT = '已接受';
+Guest.ALL = '全部';
+Guest.prototype.accept = function () {
+    this.state = Guest.ACCEPT;
+}
+Guest.prototype.refuse = function () {
+    this.state = Guest.REFUSE;
+}
