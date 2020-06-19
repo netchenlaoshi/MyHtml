b4e8ea531fa3c63446b038223c579e0bfc1ddbe3
hql752968851 <2521253602@qq.com>
2020-06-19 09:52:09 +0800

    12

---
+++ 14黄晴朗/js/ModelService.js
@@ -0,0 +1,43 @@
+var model = angular.module('nameList.model', []);
+
+function Guest(name, phone) {
+    this.name = name;
+    this.phone = phone;
+    this.state = Guest.INVITE;
+}
+
+
+Guest.ACCEPT = '已接受';
+Guest.REFUSE = '已拒绝';
+Guest.ALL = '全部';
+Guest.prototype.accept = function () {
+    this.state = Guest.ACCEPT;
+}
+
+
+model.factory('modelService', function() {
+    var guestList = {
+        list: [],
+        add: function (name,phone) {
+            var guest = new Guest(name,phone);
+            this.list.push(guest);
+        },
+
+        remove: function (guest) {
+            this.list = this.list.filter(function (item) {
+                return guest.phone != item.phone;
+            })
+        },
+
+        getList: function () {
+                return this.list.filter(function(){
+                    return true;
+                 })
+                 return this.list.filter(function(guest){
+                     return guest.state==state;
+                 })
+ 
+        }
+    }
+    return guestList;
+})
