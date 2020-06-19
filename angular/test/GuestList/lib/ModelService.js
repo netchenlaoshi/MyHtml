ca41d5ff2db45ebb63c379c34947cb7b36c68c32
fanbin <1953491515@qq.com>
2020-06-19 10:33:27 +0800

    last test

---
+++ angular/test/GuestList/lib/ModelService.js
@@ -0,0 +1,26 @@
+var model=angular.module('nameList.model',[]);
+
+
+function Guest(name,phone){
+    this.name=name;
+    this.phone=phone;
+}
+
+
+model.factory('modelService',function(){
+    var guestList={
+        list:[],
+        add:function(name,phone){
+            var guest=new Guest(name,phone);
+            this.list.push(guest);
+        },
+        getList:function(state){
+            if(state ==Guest.ALL){
+                return this.list.filter(function(){
+                    return true;
+                })
+            }
+        }
+    }
+    return guestList;
+})
