d2c8ec4cb277ac0f315aaee7d76f03be562f0034
BountyHunter12138 <761670147@qq.com>
2020-06-19 09:57:39 +0800

    期末考试

--- angular/test/8/js/ModelService.js
+++ angular/test/8/js/ModelService.js
@@ -1,24 +1,24 @@
-var model=angular.module('nameList.model',[]);
+var model = angular.module('nameList.model', []);
 
-function Guest(name,phone){
-    this.name=name;
-    this.phone=phone;
-    this.state=Guest.INVITE
+function Guest(name, phone) {
+    this.name = name;
+    this.phone = phone;
+    this.state = Guest.INVITE
 }
 
 Guest.INVITE = '邀请中';
 Guest.ACCEPT = '已接受';
 
 
-Guest.prototype.accept = function () {
+Guest.prototype.accept = function() {
     this.state = Guest.ACCEPT;
 }
 
 
-model.factory('modelService',function(){
+model.factory('modelService', function() {
     var guestList = {
         list: [],
-        add: function (name, phone) {
+        add: function(name, phone) {
             var guest = new Guest(name, phone);
             this.list.push(guest);
             return {
@@ -27,14 +27,14 @@ model.factory('modelService',function(){
             }
         },
 
-        getList:function(state){
-            if(state ==Guest.ALL){
-                return this.list.filter(function(){
+        getList: function(state) {
+            if (state == Guest.ALL) {
+                return this.list.filter(function() {
                     return true;
                 })
             }
-            return this.list.filter(function (guest){
-                return guest.state==state;
+            return this.list.filter(function(guest) {
+                return guest.state == state;
             })
         }
     }
