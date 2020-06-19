8eb900b8921f14058c1ecdd5eb8c4822cba196b7
sjwkkkk <61403933+sjwkkkk@users.noreply.github.com>
2020-06-19 15:54:57 +0800

    Add files via upload

---
+++ angular/test/songjiewen/spiders/27songjiewen.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from songjiewe.items import SongjiewenItem
+
+class A12jifanSpider(scrapy.Spider):
+    name = '27songjiewen'
+    allowed_domains = ['quote.stockstar.com']
+    start_urls = ['http://quote.stockstar.com/stock/blockperformance_0_400129614_0_0_1.html']
+
+    def parse(self, response):
+        for i in range(1,33,1):
+            self.start_urls.append('http://quote.stockstar.com/stock/blockperformance_0_400129614_0_0_{}.html'.format(i))
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.stock_of_page)
+
+    def stock_of_page(self, response):
+        for i in range(1, 32, 1):
+            yield (self.each_stock(response, i))
+
+    def each_stock(self, response, i):
+        item = Songjiewen()
+        item['code'] = response.xpath('//*[@id="datalist"]/tr[{}]/td[1]/a/text()'.format(i)).extract()
+        item['name'] = response.xpath('//*[@id="datalist"]/tr[{}]/td[2]/a/text()'.format(i)).extract()
+        item['liutongshizhi'] = response.xpath('//*[@id="datalist"]/tr[{}]/td[3]/text()'.format(i)).extract()
+        item['zongshizhi'] = response.xpath('//*[@id="datalist"]/tr[{}]/td[4]/text()'.format(i)).extract()
+        item['liutongguben'] = response.xpath('//*[@id="datalist"]/tr[{}]/td[5]/text()'.format(i)).extract()
+        item['zongguben'] = response.xpath('//*[@id="datalist"]/tr[{}]/td[6]/text()'.format(i)).extract()
+        return item
