8050c2207306e94a1ace543ed8ddc3352b11e524
ASHRekindle <2099750696@qq.com>
2020-06-19 09:58:01 +0800

    作业

---
+++ GuestList/js/ModelService1.js
@@ -0,0 +1,43 @@
+var model = angular.module('nameList.model', []);
+
+function Guest(name, phone) {
+    this.name = name;
+    this.phone = phone;
+    this.state = Guest.INVITE;
+}
+
+Guest.ACCEPT = '已接受';
+Guest.prototype.accept = function () {
+    this.state = Guest.ACCEPT;
+}
+model.factory('modelService', function () {
+    var guestList = {
+        list: [],
+        add: function (name, phone) {
+            var guest = new Guest(name, phone);
+            this.list.push(guest);
+            return {
+                code: 0,
+                guest: guest
+            }
+        },
+
+        remove: function (guest) {
+            this.list = this.list.filter(function (item) {
+                return guest.phone != item.phone;
+            })
+        },
+
+        getList:function(state){
+            if (state==Guest.ALL){
+                return this.list.filter(function(){
+                    return true;
+                })
+            }
+            return this.list.filter(function (guest){
+                return guest.state==state;
+            })
+        }
+    }
+    return guestList;
+})
